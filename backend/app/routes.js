f7c8f714beb3ce3ccfe3cadca3d0784e7d919f03
Faizan <fmouhamad@hotmail.fr>
2020-04-18 22:03:27 +0200

    add update user method and create path

--- backend/app/routes.js
+++ backend/app/routes.js
@@ -1,4 +1,5 @@
 let userService = require('./services/user.services');
+let mongoose = require('mongoose')
 
 module.exports = function (app) {
   
@@ -41,5 +42,28 @@ module.exports = function (app) {
         })
     })
 
-    
+    app.post('/getUserById', (req, res) => {
+        let _id = mongoose.Types.ObjectId(req.body.id)
+        userService
+        .getUserById(_id)
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
+    });
+
+    app.put('/updateUser', (req, res) => {
+        console.log(req.body)
+        let _id = mongoose.Types.ObjectId(req.body.id);
+        userService
+        .updateUser(req.body.user, _id)
+        .then(result => {
+            res.status(200).json('User was successfully updated');
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
+    })
 }
--- backend/app/services/user.services.js
+++ backend/app/services/user.services.js
@@ -27,21 +27,21 @@ function getUsers() {
 function createUser(user) {
     return new Promise((resolve, reject) => {
         getUser(user.email)
-        .then(result => {
-            if(result){
-                reject('This email is already taken !');
-            }
-            else {
-                const newUser = new User(user);
-                newUser.save()
-                .then(res => {
-                    resolve(res);
-                })
-                .catch(err => {
-                    reject(err);
-                })
-            }
-        })
+            .then(result => {
+                if (result) {
+                    reject('This email is already taken !');
+                }
+                else {
+                    const newUser = new User(user);
+                    newUser.save()
+                        .then(res => {
+                            resolve(res);
+                        })
+                        .catch(err => {
+                            reject(err);
+                        })
+                }
+            })
     })
 }
 
@@ -66,9 +66,40 @@ function deleteUser(email) {
     })
 }
 
+function getUserById(id) {
+    return new Promise((resolve, reject) => {
+        User.findById(id)
+            .then(user => {
+                if (user) {
+                    resolve(user);
+                }
+                else {
+                    reject('No user found')
+                }
+            })
+            .catch(err => {
+                reject(err);
+            });
+    });
+}
+
+function updateUser(user, id){
+    return new Promise((resolve, reject) => {
+        User.findByIdAndUpdate(id, user)
+        .then(res => {
+            resolve(res);
+        })
+        .catch(err => {
+            reject(err)
+        })
+    })
+}
+
 module.exports = {
     getUser,
     getUsers,
     createUser,
-    deleteUser
+    deleteUser,
+    getUserById,
+    updateUser
 }
