6a8faca044fecb6a695b59ee36b4ace0ed24622a
Faizan <fmouhamad@hotmail.fr>
2020-04-18 20:07:17 +0200

    display message when add new user

--- backend/app/routes.js
+++ backend/app/routes.js
@@ -12,7 +12,7 @@ module.exports = function (app) {
         userService
         .createUser(req.body)
         .then(result => {
-            res.status(200).json(result);
+            res.status(200).json("User has successfully created");
         })
         .catch(err => {
             res.status(400).json(err);
@@ -31,7 +31,6 @@ module.exports = function (app) {
     })
 
     app.delete('/deleteUser', (req, res) => {
-        console.log(req.body)
         userService
         .deleteUser(req.body.user.email)
         .then(result => {
--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -11,10 +11,14 @@ import {Router} from "@angular/router";
 })
 export class AddUserComponent implements OnInit {
   addForm;
+  loading: boolean = false;
   showMsg: boolean = false;
+  showErr: boolean = false;
+  res_message = null;
   constructor(
     private formBuilder: FormBuilder,
-    private userService: UserlistService
+    private userService: UserlistService,
+    private router: Router
   ) { 
     this.addForm = this.formBuilder.group({
       'lastname' : '',
@@ -28,10 +32,18 @@ export class AddUserComponent implements OnInit {
   }
 
   onSubmit(user) {
+    this.showMsg = false;
+    this.showErr = false;
+    this.loading = false;
     this.userService.createUser(user).subscribe(res => {
       this.showMsg = true;
+      this.res_message = res;
+      this.loading = true;
+      setTimeout(() => {this.router.navigate(['/'])}, 3000)
+    }, (err) => {
+      this.showErr = true;
+      this.res_message = err.error;
     });
-    
   }
 
 }
